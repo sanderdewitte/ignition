1bb443bf42f783b64cb23b3a3dbfde79fd48c438
Sander de Witte <github@sanderdewitte.com>
2022-01-16 20:14:46 +0700

    Deleted coreos-env.sh

--- scripts/coreos-env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cat >/etc/sysconfig/coreos-env <<EOF
-# Docker group ID (used by netdata)
-DOCKER_GROUP_ID=$(id -g docker)
-
-# Server domain namee
-SERVER_DOMAIN_NAME=${SERVER_DOMAIN_NAME}
-EOF
