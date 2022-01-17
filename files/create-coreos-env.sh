1230ed800cb90ee6fec1049ed4323a2e04e944e8
sdewitte <info@sanderdewitte.com>
2022-01-17 07:34:58 +0100

    Renamed coreos-env.sh to create-coreos-env.sh, updated base.bu
    Updated command to get docker group id and added exit code in create-coreos-env.sh, changed create-coreos-env.sh location on server from /opt/bootstrap to /usr/local/bin (SELinux does not allow to run scripts in /opt), changed mode from 0755 to 0750

--- files/create-coreos-env.sh
+++ files/create-coreos-env.sh
@@ -2,8 +2,14 @@
 
 cat >/etc/sysconfig/coreos-env <<EOF
 # Docker group ID (used by netdata)
-DOCKER_GROUP_ID=$(id -g docker)
+DOCKER_GROUP_ID=$(getent group docker | cut -d ':' -f 3)
 
 # Server domain name
 SERVER_DOMAIN_NAME=${SERVER_DOMAIN_NAME}
 EOF
+
+if [ ! -f /etc/sysconfig/coreos-env ]; then
+  exit 1
+fi
+
+exit 0
