5f50c394f867731578ec46e20ac92faab00c3b61
sdewitte <info@sanderdewitte.com>
2022-01-17 11:12:30 +0100

    Updated base.bu, sdewitte-vps.bu, create-coreos-env.sh script and .gitignore
    Added deployment of Vultr API key

--- files/create-coreos-env.sh
+++ files/create-coreos-env.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 cat >/etc/sysconfig/coreos-env <<EOF
-# Docker group ID (used by netdata)
+# Docker group ID (used by netdata container)
 DOCKER_GROUP_ID=$(getent group docker | cut -d ':' -f 3)
 
-# Server domain name
+# Server domain name (used by all containers)
 SERVER_DOMAIN_NAME=${SERVER_DOMAIN_NAME}
+
+# Vultr API key file location (used by traefik container)
+VULTR_API_KEY_FILE=/root/.secret/vultr_api.key
 EOF
 
 if [ ! -f /etc/sysconfig/coreos-env ]; then
