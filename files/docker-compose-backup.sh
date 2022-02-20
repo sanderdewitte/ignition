65ad3051fb3a6db3a7d89f237c262a708296c424
sdewitte <info@sanderdewitte.com>
2022-02-20 16:27:03 +0100

    Updated docker-compose-backup.sh script
    Updated list of volumes to skip

--- files/docker-compose-backup.sh
+++ files/docker-compose-backup.sh
@@ -75,7 +75,7 @@ for service_name in $(docker-compose config --services); do
   docker logs "$container_id" > "$service_dir/docker.out" 2> "$service_dir/docker.err"
 
   # save data volumes
-  skip_volumes=("/var/run/docker.sock" "/sys" "/proc" "/data/backups")
+  skip_volumes=("/var/run/docker.sock" "/sys" "/proc" "/var/opt/data/backups")
   mkdir -p "$service_dir/volumes"
   for source in $(docker inspect -f '{{range .Mounts}}{{println .Source}}{{end}}' "$container_id"); do
     match=0 
