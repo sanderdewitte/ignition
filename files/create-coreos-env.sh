db1f4761980d570fb8e3a0b90ffc16815bcf5b95
sdewitte <info@sanderdewitte.com>
2023-11-03 11:55:54 +0100

    Updated create-coreos-env.sh script
    Added setting CONTAINER_USER_ID and CONTAINER_GROUP_ID variables

--- files/create-coreos-env.sh
+++ files/create-coreos-env.sh
@@ -7,6 +7,12 @@ CORE_USER_ID=$(getent passwd core | cut -d ':' -f 3)
 # Docker group ID
 DOCKER_GROUP_ID=$(getent group docker | cut -d ':' -f 3)
 
+# Container user ID
+CONTAINER_USER_ID=$(getent passwd | grep container-user | head -1 | cut -d ':' -f 3)
+
+# Container group ID
+CONTAINER_GROUP_ID=$(getent group | grep container-group | head -1 | cut -d ':' -f 3)
+
 # Server domain name
 SERVER_DOMAIN_NAME=${SERVER_DOMAIN_NAME}
 
