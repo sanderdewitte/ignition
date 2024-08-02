c5126574add42ce00da8d0de0539b7ed18edf402
Sander de Witte <github@sanderdewitte.com>
2024-08-02 14:20:30 +0200

    Updated create-coreos-env.sh script
    Adding (optional) additional domain names for services (with environmental variables named "ADDITIONAL_DOMAIN_NAME_01" to "ADDITIONAL_DOMAIN_NAME_99")

--- files/create-coreos-env.sh
+++ files/create-coreos-env.sh
@@ -13,12 +13,29 @@ CONTAINER_USER_ID=$(getent passwd | grep container-user | head -1 | cut -d ':' -
 # Container group ID
 CONTAINER_GROUP_ID=$(getent group | grep container-group | head -1 | cut -d ':' -f 3)
 
-# Server domain name
-SERVER_DOMAIN_NAME=${SERVER_DOMAIN_NAME}
-
 # Server timezone
 TZ=${TZ}
+
+# Server domain name
+SERVER_DOMAIN_NAME=${SERVER_DOMAIN_NAME}
 EOF
+
+# Initialize a variable to hold additional domain names
+ADDITIONAL_DOMAIN_NAMES=""
+
+# Loop through potential additional domain names
+for i in $(seq -w 1 99); do
+  VAR_NAME="ADDITIONAL_DOMAIN_NAME_$i"
+  VAR_VALUE=${!VAR_NAME}
+  if [ -n "$VAR_VALUE" ]; then
+    ADDITIONAL_DOMAIN_NAMES+="${VAR_NAME}=${VAR_VALUE}\n"
+  fi
+done
+
+# Append ADDITIONAL_DOMAIN_NAMES (if any)
+if [ -n "$ADDITIONAL_DOMAIN_NAMES" ]; then
+  echo -e "\n# Additional domain name(s)\n$ADDITIONAL_DOMAIN_NAMES" >> /etc/sysconfig/coreos.env
+fi
 
 if [ ! -f /etc/sysconfig/coreos.env ]; then
   exit 1
